cf0ba966b1e176dd821e7ac04e393fca8ed0dad1
St Louis County <blord-castillo@stlouisco.com>
2014-01-29 15:40:51 -0600

    Create Assignment1.py
    Assignment 1. We will work on this in class.

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+# Your name here
+# Assignment 1: Batch Geoprocessing
+
+import arcpy
+
+def main(shapefiles,prefix):
+    """Buffer a list of input shapefiles, prefixing the new files."""
+    pass
+
+if __name__ == '__main__':
+    s = []
+    prefix = u''
+    main()
