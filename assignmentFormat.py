c798846fc82c4b756fb507afb09dd59002e0ecfd
St Louis County <blord-castillo@stlouisco.com>
2014-01-29 15:36:47 -0600

    Create assignmentFormat.py
    This is the general format for assignments without class definitions.

---
+++ assignmentFormat.py
@@ -0,0 +1,22 @@
+# Brett Lord-Castillo (Place your name here in a comment)
+# Assignment 0: Assignment Format Sample (Assignment number and title go here in a comment)
+
+# Place your imports at the top of your code, one per line.
+import sys
+import arcpy
+
+# Place your code in a main() function.
+# Comment in complete sentences.  Use two spaces between sentences.
+def main(user):
+    """Say "Hello world" and say "Hello" to user."""
+    #Include a docstring as above.
+    #Use four spaces per indent.  Ident your comments to match code ident.
+    print 'Hello world. Hello', name
+
+# Call your main function when program is executed.
+# This allows import without executing the main function
+if __name__ == '__main__':
+    main()
+
+# Style guide is available at PEP 8 http://www.python.org/dev/peps/pep-0008/
+# And Docstring style guide is available at PEP 257 http://www.python.org/dev/peps/pep-0257/
