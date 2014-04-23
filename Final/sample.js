d48c17d0da89ed5ed8dfd826a2c211a978ee99d4
St Louis County <blord-castillo@stlouisco.com>
2014-04-23 15:39:36 -0500

    Sample data for the final

---
+++ Final/sample.js
@@ -0,0 +1,8 @@
+[
+	{"site": "A-00007", "blgnumber": "095","building": "Rudolph Hall      ", "lat": 38.64908, "long": -90.30445, "locatorcolumn": "J", "locatorrow": "6", "type": "B"},
+	{"site": "A-00003", "blgnumber": "023","building": "Compton Hall      ", "lat": 38.64934, "long": -90.30510, "locatorcolumn": "J", "locatorrow": "6", "type": "G"},
+	{"site": "A-00006", "blgnumber": "024","building": "Crow Hall         ", "lat": 38.64900, "long": -90.30526, "locatorcolumn": "J", "locatorrow": "6", "type": "N"},
+	{"site": "A-00002", "blgnumber": "125","building": "Whitaker Hall     ", "lat": 38.64908, "long": -90.30320, "locatorcolumn": "K", "locatorrow": "6", "type": "A"},
+	{"site": "A-00001", "blgnumber": "015","building": "Brookings Hall    ", "lat": 38.64804, "long": -90.30502, "locatorcolumn": "J", "locatorrow": "7", "type": "G"},
+	{"site": "A-00004", "blgnumber": "010","building": "Beaumont Pavillion", "lat": 38.64848, "long": -90.30551, "locatorcolumn": "I", "locatorrow": "6", "type": "B"}
+]
